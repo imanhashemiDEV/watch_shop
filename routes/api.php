fdc1c50d9c3daa334464bca1dd26c7e000cec27d
iman <hashemi.iman@gmail.com>
2022-05-24 21:57:04 +0430

    routes refactor

--- routes/api.php
+++ routes/api.php
@@ -6,62 +6,58 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/v1')->namespace('Api\V1')->middleware('auth:sanctum')->group(function () {
 
+    //===================== User =====================//
     // register user name and address
     Route::post('register', [\App\Http\Controllers\Api\V1\AuthApiController::class, 'register']);
-
     //    user profile
     Route::post('profile', [\App\Http\Controllers\Api\V1\UserApiController::class, 'profile']);
-
     // get user addresses
     Route::post('user_addresses', [\App\Http\Controllers\Api\V1\UserApiController::class, 'userAddresses']);
 
+    //===================== Order =====================//
     // payment
-    Route::post('payment',[\App\Http\Controllers\Api\V1\PaymentController::class,'payment'])->name('save.payment');
+    Route::post('payment', [\App\Http\Controllers\Api\V1\PaymentController::class, 'payment'])->name('save.payment');
 
+    //===================== Product =====================//
     // save product comment
     Route::post('save_product_comment', [\App\Http\Controllers\Api\V1\ProductPageApiController::class, 'saveComment']);
-
 });
 
 
 Route::prefix('/v1')->namespace('Api\V1')->group(function () {
 
+    //===================== Auth =====================//
     // Send sms  to verify user
     Route::post('send_sms', [\App\Http\Controllers\Api\V1\AuthApiController::class, 'sendSms']);
-
     // check recieved sms
     Route::post('check_sms_code', [\App\Http\Controllers\Api\V1\AuthApiController::class, 'checkSmsCode']);
 
+    //===================== Home =====================//
     // get home page
     Route::get('home', [\App\Http\Controllers\Api\V1\HomePageApiController::class, 'home']);
 
+    //===================== Product =====================//
     // get all products
     Route::get('all_products', [\App\Http\Controllers\Api\V1\ProductPageApiController::class, 'allProducts']);
-
     // get newest products
     Route::get('newestl_products', [\App\Http\Controllers\Api\V1\ProductPageApiController::class, 'newestProducts']);
-
     // get most expensive products
     Route::get('most_expensive_products', [\App\Http\Controllers\Api\V1\ProductPageApiController::class, 'mostExpensiveProducts']);
-
     // get most viewed products
     Route::get('most_viewed_products', [\App\Http\Controllers\Api\V1\ProductPageApiController::class, 'mostViewedProducts']);
-
     // get cheapest products
     Route::get('cheapest_products', [\App\Http\Controllers\Api\V1\ProductPageApiController::class, 'cheapestProducts']);
-
     // get products by category
     Route::get('products_by_category/{id}', [\App\Http\Controllers\Api\V1\ProductPageApiController::class, 'productsByCategory']);
-
     // get products by brand
     Route::get('products_by_brand/{id}', [\App\Http\Controllers\Api\V1\ProductPageApiController::class, 'productsByBrand']);
-
     // get product details
     Route::get('product_details/{id}', [\App\Http\Controllers\Api\V1\ProductPageApiController::class, 'productDetail']);
-
-    // payment callback
-    Route::get('payment/callback',[\App\Http\Controllers\Api\V1\PaymentController::class,'callback']);
-
     // search product
     Route::post('search_product', [\App\Http\Controllers\Api\V1\ProductPageApiController::class, 'searchProduct']);
+    
+    //===================== Order =====================//
+    // payment callback
+    Route::get('payment/callback', [\App\Http\Controllers\Api\V1\PaymentController::class, 'callback']);
+
 });
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 
 // Front routes
+//===================== Home =====================//
 Route::get('/', function () {return view('front.home');})->name('home');
+
+//===================== Dashboard =====================//
 Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 
 //Admin routes
 Route::prefix('admin')->middleware(['auth:sanctum', 'verified', 'admin'])->group(function (){
 
+    //===================== Dashboard =====================//
     Route::get('/',[\App\Http\Controllers\Admin\PanelController::class,'index'])->name('admin.panel');
+
+    //===================== Product =====================//
     Route::resource('/categories', \App\Http\Controllers\Admin\CategoryController::class);
     Route::resource('/sliders', \App\Http\Controllers\Admin\SliderController::class);
     Route::resource('/brands', \App\Http\Controllers\Admin\BrandController::class);
@@ -18,15 +24,6 @@ Route::prefix('admin')->middleware(['auth:sanctum', 'verified', 'admin'])->group
     Route::resource('/properties', \App\Http\Controllers\Admin\PropertyController::class);
     Route::resource('/property_group', \App\Http\Controllers\Admin\PropertyGroupController::class);
     Route::resource('/colors', \App\Http\Controllers\Admin\ColorController::class);
-    Route::resource('/users', \App\Http\Controllers\Admin\UserController::class);
-    Route::get('/create_user_roles/{id}',[\App\Http\Controllers\Admin\UserController::class,'createUserRoles'])->name('create.user.roles');
-    Route::post('/store_user_roles/{id}',[\App\Http\Controllers\Admin\UserController::class,'storeUserRoles'])->name('store.user.roles');
-    Route::resource('/roles',\App\Http\Controllers\Admin\RoleController::class);
-    Route::get('/create_role_permissions/{id}',[\App\Http\Controllers\Admin\RoleController::class,'createRolePermission'])->name('create.role.permission');
-    Route::post('/store_role_permissions/{id}',[\App\Http\Controllers\Admin\RoleController::class,'storeRolePermission'])->name('store.role.permission');
-    Route::resource('/permissions',\App\Http\Controllers\Admin\PermissionController::class);
-    Route::resource('/tags',\App\Http\Controllers\Admin\TagController::class);
-    Route::get('/logs', [\App\Http\Controllers\Admin\LogViewerController::class,'index'])->name('logs.index');
     Route::resource('/galleries',\App\Http\Controllers\Admin\GalleryController::class);
     Route::get('/comments',[\App\Http\Controllers\Admin\CommentController::class,'index'])->name('comments.index');
     Route::delete('/comments/{id}',[\App\Http\Controllers\Admin\CommentController::class,'destroy'])->name('comment.destroy');
@@ -36,6 +33,19 @@ Route::prefix('admin')->middleware(['auth:sanctum', 'verified', 'admin'])->group
     Route::delete('/delete_product_gallery/{id}',[\App\Http\Controllers\Admin\GalleryController::class,'deleteGallery'])->name('delete.product.gallery');
     Route::get('/create_product_properties/{id}',[\App\Http\Controllers\Admin\ProductController::class,'createProductProperties'])->name('create.product.properties');
     Route::post('/store_product_properties/{id}',[\App\Http\Controllers\Admin\ProductController::class,'storeProductProperties'])->name('store.product.properties');
+    Route::resource('/tags',\App\Http\Controllers\Admin\TagController::class);
+
+    //===================== User =====================//
+    Route::resource('/users', \App\Http\Controllers\Admin\UserController::class);
+    Route::get('/create_user_roles/{id}',[\App\Http\Controllers\Admin\UserController::class,'createUserRoles'])->name('create.user.roles');
+    Route::post('/store_user_roles/{id}',[\App\Http\Controllers\Admin\UserController::class,'storeUserRoles'])->name('store.user.roles');
+    Route::resource('/roles',\App\Http\Controllers\Admin\RoleController::class);
+    Route::get('/create_role_permissions/{id}',[\App\Http\Controllers\Admin\RoleController::class,'createRolePermission'])->name('create.role.permission');
+    Route::post('/store_role_permissions/{id}',[\App\Http\Controllers\Admin\RoleController::class,'storeRolePermission'])->name('store.role.permission');
+    Route::resource('/permissions',\App\Http\Controllers\Admin\PermissionController::class);
+    Route::get('/logs', [\App\Http\Controllers\Admin\LogViewerController::class,'index'])->name('logs.index');
+
+    //===================== Order =====================//
     Route::get('/orders',[\App\Http\Controllers\Admin\OrderController::class,'orders'])->name('orders.index');
     Route::get('/order_details/{id}',[\App\Http\Controllers\Admin\OrderController::class,'orderDetails'])->name('order.details');
 });
